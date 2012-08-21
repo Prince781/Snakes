1ae672c1ea4e0e372fb72fffc084ebbea4f59308
Prince781 <princetonf@optonline.net>
2012-08-21 11:36:58 -0400

    Almost done with pickup support...

--- js/game.js
+++ js/game.js
@@ -1018,9 +1018,6 @@ function SnakesGame(){ //must be called using the "new" JavaScript keyword
 							cx.closePath();
 							cx.lineWidth = 1;
 							pkGradient();
-							var v = parseFloat(Math.sin((gThis.g.gt-gThis.g.pk[i].i/500)*Math.PI).toFixed(14)); //variation
-							for (var x=pk.x-8; x<=pk.x+8; x++)
-								
 							break;
 					}
 				}
