bef701f5de990fd2e790bb62874d46cd25ed696f
Prince781 <princetonf@optonline.net>
2012-11-26 22:01:41 -0500

    Updating security since some blokes decided to have a little fun with my data tables.

--- submit.php
+++ submit.php
@@ -7,8 +7,12 @@ if (isset($_POST["username"]) && isset($_POST["score"]) && isset($_POST["level"]
 	$username = $_POST["username"];
 	$score = $_POST["score"];
 	$level = $_POST["level"];
-} else exit("There's nothing to see here...");
+} elseif (!$_SERVER['SERVER_ADDR'] == $_SERVER['REMOTE_ADDR'])
+	exit("Hmm....so you thought you could hack using this method?");
+  else exit("There's nothing to see here...");
 //otherwise, perform
+if (preg_match("/\s/",$username.$score.$level) || preg_match("/\W/i",$username.$score.$level))
+	exit("Improper values given.");
 $db_pass = "";
 $xml_data = simplexml_load_file("../../../server_info/server.conf.xml");
 foreach ($xml_data->children() as $user) //get users from userlist
